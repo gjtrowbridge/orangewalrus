21205354fd7410f26b6025671366c36cf8dd18eb
Greg Trowbridge <gjtrowbridge@gmail.com>
2014-07-07 14:28:39 -0700

    Added basic front-end routing with ui-router

--- public/app/app.js
+++ public/app/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('orangeWalrus', []);
+var app = angular.module('orangeWalrus', ['ui.router']);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('activities', {
+      url: "/activities",
+      templateUrl: 'app/activities/activities.html'
+    })
+    .state('activity', {
+      url: "/activity",
+      templateUrl: "/app/activities/activity.html"
+    });
+
+});
